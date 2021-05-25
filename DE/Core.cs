0a774b9e84002baf1403367e72499a5f969e61b4
WHITE-LIME <dimakuraedov@gmail.com>
2021-05-25 16:24:42 +0300

    First commit

---
+++ DE/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DE
+{
+    class Core
+    {
+        public static kkuraedovEntities DB = new kkuraedovEntities();
+    }
+}
---
+++ DE/MainWindow.xaml.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DE
+{
+    public partial class Product
+    {
+        public Uri ImagePreview
+        {
+            get
+            {
+                var imageName = Environment.CurrentDirectory + Image ?? "";
+                return System.IO.File.Exists(imageName) ? new Uri(imageName) : new Uri("pack://application:,,,/products/picture.png");
+            }
+        }
+    }
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        private IEnumerable<Product> _ProductList;
+
+        private int SortType = 0;
+        public string[] SortList { get; set; } = {
+            "Без сортировки",
+            "Название (А-Я)",
+            "Название (Я-А)",
+            "Цена по возрастанию",
+            "Цена по убыванию",
+            "Сначала с картинками",
+            "Сначала без картинок" };
+
+        private int _CurrentPage = 1;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int CurrentPage
+        {
+            get
+            {
+                return _CurrentPage;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    if ((_ProductList.Count() % 20) == 0)
+                    {
+                        if (value <= _ProductList.Count() / 20)
+                        {
+                            _CurrentPage = value;
+                            Invalidate();
+                        }
+                    }
+                    else
+                    {
+                        if (value <= (_ProductList.Count() / 20) + 1)
+                        {
+                            _CurrentPage = value;
+                            Invalidate();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Invalidate()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ProductList"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentPage"));
+        }
+
+        public IEnumerable<Product> ProductList
+        {
+            get
+            {
+                var Result = _ProductList;
+
+                switch (SortType)
+                {
+                    case 1:
+                        Result = Result.OrderBy(p => p.Title);
+                        break;
+                    case 2:
+                        Result = Result.OrderByDescending(p => p.Title);
+                        break;
+                    case 3:
+                        Result = Result.OrderBy(p => p.MinCostForAgent);
+                        break;
+                    case 4:
+                        Result = Result.OrderByDescending(p => p.MinCostForAgent);
+                        break;
+                    case 5:
+                        Result = Result.OrderByDescending(p => p.Image);
+                        break;
+                    case 6:
+                        Result = Result.OrderBy(p => p.Image);
+                        break;
+                }
+
+                return Result.Skip((CurrentPage - 1) * 20).Take(20);
+            }
+            set
+            {
+                _ProductList = value;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            ProductList = Core.DB.Product.ToArray();
+        }
+
+
+        
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void PrevPage_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentPage--;
+        }
+
+        private void NextPage_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentPage++;
+        }
+
+        private void SortTypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SortType = SortTypeComboBox.SelectedIndex;
+            Invalidate();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var addWindow = new AddAndEditWindow(new Product());
+            if(addWindow.ShowDialog() == true)
+            {
+                ProductList = Core.DB.Product.ToArray();
+            }
+
+        }
+
+        private void ProductListView_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var EditTourWindow = new AddAndEditWindow(ProductListView.SelectedItem as Product);
+            if (EditTourWindow.ShowDialog() == true)
+            {
+                ProductList = Core.DB.Product.ToArray();
+            }
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            var w = new MainWindow();
+            w.Show();
+
+            this.Close();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var DeleteProduct = ProductListView.SelectedItem as Product;
+            try
+            {
+                Core.DB.Product.Remove(DeleteProduct);
+                Core.DB.SaveChanges();
+
+                MessageBox.Show($"Удалено!");
+
+                ProductList = Core.DB.Product.ToArray();
+
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("ProductList"));
+                }
+            }
+            catch { }
+
+        }
+    }
+}
